3ab90afd0b8ae58b3cda33664f1f3cfc07821730
Wiedenbruch-Invision <detlef.wiedenbruch@invision.de>
2013-04-03 15:45:46 +0200

    recent changes

---
+++ spec/controllers/matches_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe MatchesController do
+  
+  let (:season) { create :best_season }
+  let (:competition) { season.competitions.first }
+
+  it "presents the dashboard" do
+  end
+  
+end
