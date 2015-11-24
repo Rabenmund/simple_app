bc267f326cec2320f66cea8a055db9ed1a98ee01
dwiedenbruch <detlef.wiedenbruch@gmx.de>
2015-11-24 17:45:32 +0100

    Fix cup game date creation issue

--- spec/lib/season_use_case/game_start_spec.rb
+++ spec/lib/season_use_case/game_start_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe SeasonUseCase::GameStart do
     expect(start.date_time(type: :league,
                            level: 1,
                            matchday_start: matchday_start,
-                           number: 1))
+                           md_number: 1,
+                           g_number: 0))
       .to eq DateTime.new(2015,8,7,20,30)
   end
 
@@ -16,7 +17,8 @@ RSpec.describe SeasonUseCase::GameStart do
     expect(start.date_time(type: :league,
                            level: 2,
                            matchday_start: matchday_start+1.day,
-                           number: 9))
+                           md_number: 1,
+                           g_number: 8))
       .to eq DateTime.new(2015,8,10,20,30)
   end
 
@@ -24,30 +26,34 @@ RSpec.describe SeasonUseCase::GameStart do
     expect(start.date_time(type: :cup,
                            level: 1,
                            matchday_start: matchday_start+7.days,
-                           number: 1))
+                           md_number: 1,
+                           g_number: 1))
       .to eq DateTime.new(2015,8,14,18,30)
   end
 
   it "has a date for the cup final" do
     expect(start.date_time(type: :cup,
-                           level: 6,
+                           level: 1,
                            matchday_start: matchday_start+294.days,
-                           number: 1))
+                           md_number: 6,
+                           g_number: 1))
       .to eq DateTime.new(2016,5,28,20,30)
   end
 
   it "has a date for the second national game of a matchday" do
     expect(start.date_time(type: :national,
                            matchday_start: matchday_start+14.days,
-                           number: 2))
+                           md_number: 2,
+                           g_number: 2))
       .to eq DateTime.new(2015,8,22,20,30)
   end
 
   it "has a date for the second half final of a national cup" do
     expect(start.date_time(type: :national_cup,
-                           level: 6,
+                           level: 1,
                            matchday_start: matchday_start+318.days,
-                           number: 2))
+                           md_number: 6,
+                           g_number: 1))
       .to eq DateTime.new(2016,6,21,20,30)
   end
 end
